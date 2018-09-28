9776253b20dc5676e6c2453fc388cd12568f6e9b
y.pernerovskyy <y.pernerovskyy@globallogic.com>
2018-09-28 11:59:35 +0300

    Add calc.html to the project

--- src/test/java/BasicScript.java
+++ src/test/java/BasicScript.java
@@ -1,16 +1,14 @@
 import java.io.File;
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.*;
 
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
+
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
+
 
 public class BasicScript {
     private static WebDriver driver;
